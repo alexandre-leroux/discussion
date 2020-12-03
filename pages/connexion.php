b226a410fbe56f1bbe2ce80170be9bc8e656d4af
prénom nom <alexandre.leroux@laplateforme.io>
2020-12-03 12:20:45 +0100

    connexion.php ok

--- pages/connexion.php
+++ pages/connexion.php
@@ -1,6 +1,6 @@
 <?php session_start();?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
 
@@ -8,31 +8,25 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
   <title>PHOTOPICS - Connexion</title>
-
-  <!-- Bootstrap core CSS -->
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
   <link href="../css/clean-blog.min.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-  <!-- Navigation -->
-  <?php
+  <!-- Navigation et fonctions utilisateur -->
+<?php
  include '../includes/nav-non-connecte.php';
  include '../fonctions/fonctions.php';
  ?>
 
   
+  <!-- test du formulaire -->
 <?php
 
 
@@ -42,11 +36,11 @@
 if ( isset($_POST['submit']))
 
       {
-          if ($_POST['login'] != NULL AND $_POST['password'] != NULL)
+          if ($_POST['login'] != NULL AND $_POST['password'] != NULL)// test si les chammps sont remplis
 
               {
 
-
+                // si ok, onteste si le login existe
                 connection_bdd();
                 $bdd = connection_bdd();
                 $requete = $bdd->prepare('SELECT * FROM utilisateurs WHERE login = :login');
@@ -54,11 +48,9 @@ if ( isset($_POST['submit']))
                 $données_utilisateur = $requete->fetch();
                 $bdd = NULL;
               
-                // echo '<pre>';
-                // print_r($données_utilisateur) ;
-                // echo '</pre>';
+            
 
-                    if ( $login == @$données_utilisateur['login'] AND password_verify($_POST['password'], $données_utilisateur['password'] ) )
+                    if ( $login == @$données_utilisateur['login'] AND password_verify($_POST['password'], $données_utilisateur['password'] ) )//si oui, on teste le password
                     {
                       
                       $_SESSION['login']=$données_utilisateur['login'];
@@ -67,12 +59,12 @@ if ( isset($_POST['submit']))
                       $connexion_reussie = 1;
                     
                     }
-                    else { echo 'Erreur de login ou de mot de passe';}
+                    else {  $erreur_login_mdp = 'Erreur de login ou de mot de passe';}
 
 
               }
 
-          else {echo 'veuillez tout remplir';}    
+          else {$champs_manquants = 'veuillez tout remplir';}    
           
       }
 
@@ -83,43 +75,24 @@ if ( isset($_POST['submit']))
 
 
   <!-- Page Header -->
-  <?php if ( !isset($connexion_reussie))
-  {?>
-  <header class="masthead" style="background-image: url('https://images.unsplash.com/photo-1605388862319-eaf1649edb5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="page-heading pb-5">
+
+  <header class="masthead vh-100" style="background-image: url('https://images.unsplash.com/photo-1605388862319-eaf1649edb5a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')">
+    <div class="overlay h-100"></div>
+    <div class="container h-100">
+      <div class="row h-100">
+        <div class="col-lg-8 col-md-10 mx-auto h-100">
+          <div class="site-heading h-100 d-flex flex-column justify-content-center">
             <h1>Connectez-vous !</h1>
             <span class="subheading">Vous avez des questions ? Nous avons les réponses.</span>
-            <img class='mt-3' src="../images/drop-down-arrow.png" alt="fleche">
+              <div> 
+                  <img class='mt-3' src="../images/drop-down-arrow.png" alt="fleche">
+              </div>
           </div>
         </div>
       </div>
     </div>
   </header>
-  <?php
-  }
-  else
-  {  ?>
-  <header class="masthead vh-100" style="background-image: url('https://images.unsplash.com/photo-1606323309671-c51570922278?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1400&q=80')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto ">
-          <div class="page-heading ">
-            <p class=' h3 p-4 text-uppercase bg-light text-success m-0 display-4'>Connexion reussie</p>
-            <span class="subheading bg-light text-primary m-0 p-2 pb-3">vous allez être redirigé vers la page d'accueil</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
-  <meta http-equiv="refresh" content="2;url=../index.php" />
-  <?php
-  }
-  ?>
+
 
 
 
@@ -127,11 +100,21 @@ if ( isset($_POST['submit']))
 
 
 <!-- ---------------------------- formulaire html ------------------------------------------- -->
+
+
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
 
-        <form name="connexion"  action="connexion.php" method="post">
+      <?php if ( isset($connexion_reussie))
+      {?>
+       <p class='text-center text-success'>Connexion reussie, vous allez être redirigé vers la page d'acueil</p>
+       <meta http-equiv="refresh" content="2;url=../index.php" />
+      <?php
+      }
+      ?>
+
+        <form name="connexion" id='form1' action="connexion.php#form1"  method="post">
           <p class=" text-center text-primary">
           </p>
           <div class="control-group">
@@ -151,6 +134,8 @@ if ( isset($_POST['submit']))
           </div>
 
           <br>
+          <p class='text-center text-danger'><?php  if (isset($erreur_login_mdp)) {echo $erreur_login_mdp;} ?></p>
+          <p class='text-center text-danger'><?php  if (isset($champs_manquants)) {echo $champs_manquants;} ?></p>
 
           <div id="success"></div>
           <button type="submit" name="submit" class="btn btn-primary" id="sendMessageButton">Se connecter</button>
@@ -158,6 +143,7 @@ if ( isset($_POST['submit']))
       </div>
     </div>
   </div>
+
 
   <hr>
 
