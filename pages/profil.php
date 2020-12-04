a014b45e034ce8a6fc3127c00f7f166f77b24b00
prénom nom <alexandre.leroux@laplateforme.io>
2020-12-04 14:25:00 +0100

    profil avance

--- pages/profil.php
+++ pages/profil.php
@@ -1,42 +1,46 @@
 <?php session_start();
 if (!isset($_SESSION['login']) and !isset($_SESSION['id'])){header('location:../index.php');}
-// echo $_SESSION['login'].'<br>';
-// echo $_SESSION['id'].'<br>';
+echo $_SESSION['login'].'<br>';
+echo $_SESSION['id'].'<br>';
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
   <title>PHOTOPICS - Profil</title>
-
-  <!-- Bootstrap core CSS -->
   <link href="../vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
   <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
-
-  <!-- Custom styles for this template -->
   <link href="../css/clean-blog.min.css" rel="stylesheet">
-
 </head>
 
 <body>
 
   <!-- Navigation -->
   <?php
+   include '../fonctions/fonctions.php';
+//pour updater le login depuis la bdd, et que l'affichage dans la nav et le menu change
+ connection_bdd();
+  $bdd = connection_bdd();
+  $requete = $bdd->prepare('SELECT login FROM utilisateurs WHERE id = :id');
+  $requete->execute(array( 'id' => $_SESSION['id']));
+  $donnees = $requete->fetchall();
+
+                               echo '<pre>';
+                               print_r($donnees) ;
+                               echo '</pre>';
+  $_SESSION['login'] = $donnees[0]['login'];
  include '../includes/nav-non-connecte.php';
- include '../fonctions/fonctions.php';
+
  ?>
 
 <?php
+
 
         if ( isset($_POST['submit']))//check bouton submit
 
